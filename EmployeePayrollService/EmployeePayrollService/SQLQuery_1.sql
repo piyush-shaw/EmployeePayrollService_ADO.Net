24092fadfa32999bfd167a18b486c0f015ef3d65
piyush-shaw <piyushshaw0000@gmail.com>
2023-02-24 12:49:45 +0530

    [Piyush] added employee payroll service problem using Ado.net

---
+++ EmployeePayrollService/EmployeePayrollService/SQLQuery_1.sql
@@ -0,0 +1,25 @@
+select * from employee_payroll
+
+Create or ALTER procedure [dbo].[SpAddEmployeeDetails]
+(
+@EmployeeName varchar(255),
+@PhoneNumber varchar(255),
+@Address varchar(255),
+@Department varchar(255),
+@Gender char(1),
+@BasicPay float,
+@Deductions float,
+@TaxablePay float,
+@Tax float,
+@NetPay float,
+@StartDate Date
+)
+as
+begin
+insert into employee_payroll values
+(
+@EmployeeName,@BasicPay,@StartDate,@Gender,@PhoneNumber,@Address,@Department,@Deductions,@TaxablePay,@Tax,@NetPay
+)
+end
+GO
+
